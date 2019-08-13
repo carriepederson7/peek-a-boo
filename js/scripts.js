ab22667a07c999a8ce0d299130d70b8f0ca8528c
Carrie Schmid and Molly Stambaugh <molly@>
2019-08-13 14:21:45 -0700

    add light mode and dark mode buttons, use add/remove class jQuery commands

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
 
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
 
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
 
   $(".clickable").click(function() {
     $("#initially-hidden").toggle();
     $("#initially-showing").toggle();
   });
-
-
-
 
   $(".clickable2").click(function() {
     $("#fadeIn").fadeToggle(4000);
